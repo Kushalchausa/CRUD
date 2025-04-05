b47dfe9af0d7895748692a5147e73d57a9b066ce
kushal <136876525+Kushalchausa@users.noreply.github.com>
2025-04-05 22:56:16 +0530

    Create CountryController.java

---
+++ src/main/java/controller/CountryController.java
@@ -0,0 +1,42 @@
+package com.assignment.country_api.controller;
+
+import com.assignment.country_api.entity.Country;
+import com.assignment.country_api.service.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/countries")
+public class CountryController {
+
+    @Autowired
+    private CountryService countryService;
+
+    @GetMapping
+    public List<Country> getAllCountries() {
+        return countryService.getAllCountries();
+    }
+
+    @GetMapping("/{id}")
+    public Country getCountryById(@PathVariable Long id) {
+        return countryService.getCountryById(id);
+    }
+
+    @PostMapping
+    public Country createCountry(@RequestBody Country country) {
+        return countryService.createCountry(country);
+    }
+
+    @PutMapping("/{id}")
+    public Country updateCountry(@PathVariable Long id, @RequestBody Country country) {
+        return countryService.updateCountry(id, country);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteCountry(@PathVariable Long id) {
+        countryService.deleteCountry(id);
+        return "Country deleted successfully";
+    }
+}
